641364ac65438c12997eedec2a29de035eba0e1a
Faical Yannick Congo <yannick.congo@gmail.com>
2022-05-10 22:47:50 +0200

    Adding actions.

---
+++ _action-update/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -l
+
+set -eu
+
+# Check hub installation
+hub version
+
+# Requires BRANCH_NAME, BOT_USER, BOT_TOKEN to be included by workflow
+export GITHUB_API_TOKEN=$BOT_TOKEN
+export DATA_TIMESTAMP=$(date -u "+%F-%H")
+
+# Configure git + hub
+git config --global user.name "${BOT_USER}"
+git config --global user.email "${BOT_USER}@users.noreply.github.com"
+git config --global hub.protocol https
+
+# Get latest copy of repository
+git clone --no-single-branch "https://${BOT_USER}:${BOT_TOKEN}@github.com/usnistgov/viz-nist-portal.git"
+cd viz-nist-portal
+REPO_ROOT=$(pwd)
+
+# Checkout data update branch, creating new if necessary
+git checkout $BRANCH_NAME || git checkout -b $BRANCH_NAME
+git merge --no-edit master
+
+# Run MASTER script
+./deploy.sh
+
+exit 0
--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env sh
 # abort on errors
 set -e
+
 # build
+git clone --branch gh-pages "https://${BOT_USER}:${BOT_TOKEN}@github.com/usnistgov/viz-nist-portal.git" /tmp/dist
+
+npm install -g @vue/cli
 yarn run build
 # navigate into the build output directory
-cd dist
+cp -r dist/* /tmp/dist/
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-git init
+cd /tmp/dist
 git add -A
-git commit -m 'deploy'
-git push -f git@github.com:usnistgov/viz-nist-portal.git master:gh-pages
+git commit -m "deployed at ${DATA_TIMESTAMP}"
+git push -f origin master:gh-pages
 cd -
