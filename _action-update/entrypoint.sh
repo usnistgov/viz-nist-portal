319f0c4aa12af2db11f261670494964495e91ccb
Faical Yannick Congo <yannick.congo@gmail.com>
2022-08-30 13:35:27 +0200

    Fixing deploy

--- _action-update/entrypoint.sh
+++ _action-update/entrypoint.sh
@@ -15,15 +15,32 @@ git config --global user.email "${BOT_USER}@users.noreply.github.com"
 git config --global hub.protocol https
 
 # Get latest copy of repository
-git clone --no-single-branch "https://${BOT_USER}:${BOT_TOKEN}@github.com/usnistgov/viz-nist-portal.git"
-cd viz-nist-portal
-REPO_ROOT=$(pwd)
+# git clone --no-single-branch "https://${BOT_USER}:${BOT_TOKEN}@github.com/usnistgov/viz-nist-portal.git"
+# cd viz-nist-portal
+# REPO_ROOT=$(pwd)
 
 # Checkout data update branch, creating new if necessary
-git checkout $BRANCH_NAME || git checkout -b $BRANCH_NAME
-git merge --no-edit master
+# git checkout $BRANCH_NAME || git checkout -b $BRANCH_NAME
+# git merge --no-edit master
 
 # Run MASTER script
-./deploy.sh
+#./deploy.sh
+
+git clone --branch gh-pages "https://${BOT_USER}:${BOT_TOKEN}@github.com/usnistgov/viz-nist-portal.git" /tmp/repo
+cd /tmp/repo
+REPO_ROOT=$(pwd)
+
+#npm install -g @vue/cli
+yarn install
+yarn run build
+# navigate into the build output directory
+cp -r dist/* /tmp/dist/
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+cd /tmp/dist
+git add -A
+git commit -m "deployed at ${DATA_TIMESTAMP}"
+git push -f origin master:gh-pages
+cd -
 
 exit 0
